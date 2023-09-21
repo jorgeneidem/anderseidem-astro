f515da1d8884061b5b59301b45196232a21cdc41
Jørgen Eidem <jorgen.eidem@gmail.com>
2023-09-21 21:44:56 +0200

    gitignore etc

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://anderseidem.no/staging/',
+	site: 'https://anderseidem.no',
 	integrations: [mdx(), sitemap()],
 });
